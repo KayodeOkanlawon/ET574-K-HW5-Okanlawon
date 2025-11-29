b194e16d086c040af05a9bc1595169d3f830fe2f
KayodeOkanlawon <kayode.okanlawon61@student.qcc.cuny.edu>
2025-11-29 03:21:11 +0000

    Add two new functions to math library (factorial, distance)

--- my_math.py
+++ my_math.py
@@ -18,3 +18,18 @@ def pow(x, y):
     for _ in range(y):
         result *= x
     return result
+
+def factorial(n):
+    """Returns n! (factorial of n)."""
+    if n < 0:
+        return "Error: factorial is not defined for negative numbers."
+
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+
+def distance(x1, y1, x2, y2):
+    """Returns the distance between two points (x1, y1) and (x2, y2)."""
+    return ((x2 - x1)**2 + (y2 - y1)**2) ** 0.5
